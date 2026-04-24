2a4a7bf25ccb016c04c4cb76e60b92b4d3d39692
samuelalbuquerque01 <samuel_albuquerque_f@hotmail.com>
2026-04-24 10:35:40 -0300

    Fix Vercel API routing

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,20 @@
+import type { IncomingMessage, ServerResponse } from "node:http";
 import app from "../server/app";
 
-export default app;
+function buildQueryString(url: URL): string {
+  const params = new URLSearchParams(url.search);
+  params.delete("route");
+  const serialized = params.toString();
+  return serialized ? `?${serialized}` : "";
+}
+
+export default function handler(req: IncomingMessage, res: ServerResponse) {
+  const requestUrl = new URL(req.url || "/api", "http://localhost");
+  const route = requestUrl.searchParams.get("route");
+
+  if (route) {
+    req.url = `/api/${route}${buildQueryString(requestUrl)}`;
+  }
+
+  return app(req as any, res as any);
+}
